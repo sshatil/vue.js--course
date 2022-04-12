244453e699850706db5929b3d584d9b9d5e99af1
sshatil <mutasimshatil@hotmail.com>
2022-04-13 03:15:03 +0800

    rendering conditional content & list

---
+++ condittionalList/app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+  data() {
+    return { goalValue: "", goals: [] };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.goalValue);
+      this.goalValue = "";
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    },
+  },
+});
+
+app.mount("#user-goals");
