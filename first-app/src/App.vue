f8b5dcc126a784a359d851d1186167fe601b6b76
sshatil <mutasimshatil@hotmail.com>
2022-04-14 01:52:02 +0800

    complete this module

---
+++ first-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h1>test</h1>
+    <ul>
+      <li>1</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "1",
+          name: "John",
+          phone: "123-123",
+          email: "john@example.com",
+        },
+        {
+          id: "1",
+          name: "juli",
+          phone: "123-123",
+          email: "juli@example.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ first-app/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreaVisible">
+      <li><strong>Phone</strong>{{ friend.phone }}</li>
+      <li><strong>Phone</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreaVisible: false,
+      friend: {
+        id: "1",
+        name: "John",
+        phone: "123-123",
+        email: "john@example.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreaVisible = !this.detailsAreaVisible;
+    },
+  },
+};
+</script>
+
+<style></style>
